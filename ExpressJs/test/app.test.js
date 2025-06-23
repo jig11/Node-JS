ec05a15d255f92ecedf758ff32538d6a072d04d8
Jignesh Shah <jignesh.a.shah@orange.com>
2025-06-23 11:10:24 +0530

    commit for triggering piepline

--- ExpressJs/test/app.test.js
+++ ExpressJs/test/app.test.js
@@ -112,6 +112,7 @@ describe('Users API (MongoDB)', () => {
 
           const user = await request(app).post('/users/signup').send(newUser);
           const res = await request(app).post('/users/login').send(newUser);
+          console.log(res.body)
           // expect(res.status).to.equal(201);
           //  expect(res.body).to.include(newCourse);
           expect(res.body).to.have.property('token');
