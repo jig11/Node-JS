5859559c085c7cebae9ccf3bc5481abdb6bf8932
Jignesh Shah <jignesh.a.shah@orange.com>
2025-06-23 11:08:37 +0530

    commit for triggering piepline

--- ExpressJs/test/app.test.js
+++ ExpressJs/test/app.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const {expect} = require('chai');
 const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');
-const app = require('../index');
+const app = require('../index'); 
 const Course = require("../schemas/courseSchema");
 const User = require("../schemas/userSchema");
 let mongoServer;
