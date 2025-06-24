f852afa2fffac5089281857adeca092ba893d31d
Jig11 <jig_11@yahoo.in>
2025-06-24 10:33:12 +0530

    added refreshtoken code but not integrated

--- ExpressJs/middleware/authenticateTokenMiddleware.js
+++ ExpressJs/middleware/authenticateTokenMiddleware.js
@@ -18,8 +18,36 @@ function authenticateToken(req, res, next) {
      return res.sendStatus(403); 
     }
     req.user = user;
+    //Verify BlackListed Access Token
+    //    redis_client.get('BL_'+decoded.sub.toString(),(err,data)=>{
+    //        if(err) throw err;
+    //        if(data===token)  return res.status(403).json({status:false,message:"Blacklisted token"})
+    //        next();
+    //    })
     next();
   });
+}
+
+function verifyRefreshToken(req,res,next){
+    const token = req.body.token;
+    if(token===null) return res.status(401).json({status:false,message:"Token is null"})
+    try {
+       const decoded = jwt.verify(token,process.env.JWT_REFRESH_SECRET);
+       console.log("Decoded value",decoded);
+       req.userData = decoded;
+       // If token is there in store or not
+
+        // redis_client.get(decoded.sub.toString(),(err,data)=>{
+        //     if(err) throw err;
+        //     if(data==null) return res.status(401).json({status:false,message:"Token is not there in store"})
+        //     if(JSON.parse(data).token != token) return res.status(401).json({status:false,message:"Token is not same in store"})
+        //     next()
+        // })
+        next()
+       
+    } catch (error) {
+        return res.status(401).json({status:false,message:"Your session is not valid"})
+    }
 }
 
 module.exports = authenticateToken; 
--- ExpressJs/routes/userRoutes.js
+++ ExpressJs/routes/userRoutes.js
@@ -7,6 +7,30 @@ const authenticateToken = require('../middleware/authenticateTokenMiddleware');
 const pinologger = require('../middleware/pinoMiddleware');
 const User = require("../schemas/userSchema");
 
+function generateRefreshToken(user_id) {
+    const refresh_token = jwt.sign(
+      { sub: user_id },
+      process.env.JWT_REFRESH_SECRET,
+      { expiresIn: process.env.JWT_REFRESH_TIME }
+    );
+
+    // redis_client.get(user_id.toString(), (err, data) => {
+    //   if (err) throw err;
+    //   redis_client.set(
+    //     user_id.toString(),
+    //     JSON.stringify({ token: refresh_token })
+    //   );
+    // });
+    return refresh_token;
+  }
+
+module.exports.GetAccessToken = (req,res)=>{
+const user_id =req.userData.sub;
+const access_token = jwt.sign({sub:user_id},process.env.JWT_ACCESS_SECRET,{expiresIn:process.env.JWT_ACCESS_TIME})
+const refresh_token = generateRefreshToken(user_id);
+return res.status(200).json({status:true,message:"Success..",data:{access_token,refresh_token}});
+}
+
 router.post("/signup", async (req, res) => {
   // Create a new user
   const hashedPassword = await bcrypt.hash(req.body.password, 10);
@@ -36,14 +60,24 @@ router.post("/login", async (req, res) => {
           }
 
           const token = jwt.sign({user}, process.env.JWT_SECRET, {expiresIn:'1h'})
+          //const access_token = jwt.sign({ sub: username },process.env.JWT_ACCESS_SECRET,{ expiresIn: process.env.JWT_ACCESS_TIME }
+          //const refresh_token = generateRefreshToken(username);
+          // token:{
+          //     access_token:access_token,
+          //     refresh_token:refresh_token
+          // }
           res.json({token});
-          } 
-          catch (err) 
-          {
-                pinologger.error('Error fetching users:', err);
-                res.status(500).send({ error: 'Failed to fetch users.' });
-          }
+        } 
+        catch (err) 
+        {
+              pinologger.error('Error fetching users:', err);
+              res.status(500).send({ error: 'Failed to fetch users.' });
+        }
 });
+
+// router.post('/login',Login);
+// router.post('/refreshToken',auth_middleware.verifyRefreshToken,GetAccessToken);
+
 
 router.post("/logout",authenticateToken, async (req, res) => {
   // logout
